f9082cc2b8de111ddcd300cb74887f4c9826014f
RaduMilici <Radu.Milici.3pg@phishme.com>
2017-12-29 00:21:50 +0200

    The wizardry of hardware acceleration

--- src/components/2_what_is_three/GpuVsCpu.js
+++ src/components/2_what_is_three/GpuVsCpu.js
@@ -8,8 +8,14 @@ class GpuVsCpu extends Component {
         <div>
           <SceneView/>
           <Card>
-            <h2>1. The basics of 3D rendering</h2>
-            <iframe width="1000" height="500" src="https://www.youtube.com/embed/-P28LKWTzrI" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+            <h2>The wizardry of hardware acceleration</h2>
+            <div style={{'max-width': '1200px'}}>
+              <p>Architecturally, the <strong>CPU</strong> is composed of just few cores that can handle a few software threads at a time.</p>
+              <p>In contrast, a <strong>GPU</strong> is composed of hundreds of cores that can handle thousands of threads simultaneously.</p>
+            </div>
+            <div style={{'text-align': 'center'}}>
+              <iframe width="1000" height="500" src="https://www.youtube.com/embed/-P28LKWTzrI" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+            </div>
           </Card>
         </div>
     );
